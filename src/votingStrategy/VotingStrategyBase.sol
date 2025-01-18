887a47527317a4f91979691c95fd86e389a6982f
Kahn <ylkk925@gmail.com>
2025-01-18 20:57:23 +0800

    resolve vote issue.

--- src/votingStrategy/VotingStrategyBase.sol
+++ src/votingStrategy/VotingStrategyBase.sol
@@ -10,13 +10,20 @@ abstract contract VotingStrategyBase is IVotingStrategy {
         address[] calldata voters,
         uint256[] calldata weights,
         address voter
-    ) internal pure returns (uint256 result) {
-        for (uint256 i = 0; i < voters.length; i++) {
-            if (voters[i] == voter) {
-                result = weights[i];
-                break;
+    ) internal pure returns (uint256) {
+        uint256 result = 0;
+        if (voters.length == weights.length) {
+            for (uint256 i = 0; i < voters.length; i++) {
+                if (voters[i] == voter) {
+                    result = weights[i];
+                    break;
+                }
             }
+        } else {
+            // error params
+            result = 1;
         }
+        return result;
     }
 
     function getResult(
